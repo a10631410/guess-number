922bff5772b1907f1305c543202682176b868c2a
Vicky <d10631410@gapps.fg.tp.edu.tw>
2020-04-08 22:24:26 +0800

    '讓玩家自己決定'

--- guess.py
+++ guess.py
@@ -1,6 +1,12 @@
 import random
-r = random.randint(1 , 100)
+start = input('請輸入你要開始的數字')
+end = input('請輸入你要結束的數字')
+start = int(start)
+end = int(end)
+r = random.randint(start , end)
+count = 0
 while True:
+	count += 1
 	num = input('請輸入數字')
 	num = int(num)
 	if num == r:
@@ -10,3 +16,4 @@ while True:
 		print('嘻嘻你猜得太大了')
 	elif num < r:
 		print('嘻嘻你猜得太小了')
+	print('這是你猜的第' , count, '次')
